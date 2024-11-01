8e410be39901a79c3055fa41664fc3dec78a178a
UbySoft <info_uby@yahoo.it>
2024-11-01 18:49:04 +0100

    .

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -3,21 +3,20 @@ import { logger } from "../utils/logger.js";
 
 // eslint-disable-next-line no-unused-vars
 export const errorHandler = (err, req, res, next) => {
-  // Utilizziamo eslint-disable-next-line per ignorare l'avviso
-  /* eslint-disable-next-line no-unused-vars */
-  logger.error(`Error: ${err.message}`, {
-    stack: err.stack,
-    method: req.method,
-    path: req.path,
-  });
+	// Utilizziamo eslint-disable-next-line per ignorare l'avviso
+	logger.error(`Error: ${err.message}`, {
+		stack: err.stack,
+		method: req.method,
+		path: req.path,
+	});
 
-  const statusCode = err.statusCode || 500;
-  error(
-    res,
-    {
-      message: err.message || "Errore interno del server",
-      ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
-    },
-    statusCode,
-  );
+	const statusCode = err.statusCode || 500;
+	error(
+		res,
+		{
+			message: err.message || "Errore interno del server",
+			...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+		},
+		statusCode
+	);
 };
